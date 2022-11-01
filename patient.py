5a9890a9a6938adec157fa557082865e0173c222
DoubleSpace <billy.lundh@gmail.com>
2022-11-01 10:23:22 +0100

    Create patient.py

---
+++ patient.py
@@ -0,0 +1,17 @@
+import medicine 
+
+class Patient:
+    def __init__(self, name, disease):
+        self.name = name
+        self.treating_disease = disease
+
+        print(name + " : " + disease)
+
+    def is_sick():
+        return
+
+    def take_medication(med):
+        print(med)
+
+    def greet(self):
+        print(self.name)
