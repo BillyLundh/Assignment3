22ec874e34c0a5439aa271b48c8c884b6201e5e5
DoubleSpace <billy.lundh@gmail.com>
2022-11-23 16:36:52 +0100

    Update patient.py

--- patient.py
+++ patient.py
@@ -3,15 +3,20 @@ import medicine
 class Patient:
     def __init__(self, name, disease):
         self.name = name
-        self.treating_disease = disease
+        self.disease = disease
 
         print(name + " : " + disease)
 
-    def is_sick():
-        return
+    def is_sick(self):
+        if self.disease == None:
+            return False
+        else:
+            return True
 
-    def take_medication(med):
-        print(med)
+    def take_medication(self, med):
+        
+        if self.disease == med.treating_disease:
+            self.disease = None
 
     def greet(self):
-        print(self.name)
+        print("Hello! " + self.name)
